c92bd8e9af20c1c608522c92bf48ed96def93f45
Ryan Cheley <9857779+ryancheley@users.noreply.github.com>
2023-04-22 20:42:39 -0700

    Update pelicanconf.py
    updated blogroll

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,16 +17,14 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
+    ("CV Firebirds", "https://cvfirebirds.com/"),
+    ("The AHL", "https://theahl.com/"),
+    ("Seattle Kraken", "https://www.nhl.com/kraken/"),
+    ("The NHL", "https://www.nhl.com/"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
 )
 
 DEFAULT_PAGINATION = 10
